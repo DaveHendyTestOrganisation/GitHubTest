2ac3b9851e57d3b65e3e34a58f0e0dd342445ef1
Dave Hendy <dave@hendy.me.uk>
2014-11-26 15:53:33 +0000

    First commit.

---
+++ src/githubtest/Constants.java
@@ -0,0 +1,19 @@
+package githubtest;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dave
+ */
+public final class Constants {
+    
+    private Constants() {}
+    
+    public static final String VERSION = "1.0";
+    
+}
