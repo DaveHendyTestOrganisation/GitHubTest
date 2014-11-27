d101ef58d09d354aba1484466b50a17e7137d202
davehendy <dave@hendy.me.uk>
2014-11-27 10:21:00 +0000

    Another comment.

--- src/githubtest/Constants.java
+++ src/githubtest/Constants.java
@@ -15,6 +15,7 @@ public final class Constants {
     private Constants() {}
     
     //Add some comment
+    //DavidHendy added another comment
     public static final String VERSION = "1.0";
     
 }
