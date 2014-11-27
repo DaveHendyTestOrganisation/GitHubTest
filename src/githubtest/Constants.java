6fbd40a43271f5406d0da0c83d24706209280715
davidhendy <david@hendy.me.uk>
2014-11-27 14:08:41 +0000

    Update version to 1.1

--- src/githubtest/Constants.java
+++ src/githubtest/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
     
     //Add some comment
     //DavidHendy added another comment
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     public static final String OWNER = "Dave";
     
