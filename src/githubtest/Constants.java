d4f3570edededacc58ca407aeb3f3c0403c11c0b
Dave Hendy <dave@hendy.me.uk>
2014-11-27 16:04:30 +0000

    Changed comment

--- src/githubtest/Constants.java
+++ src/githubtest/Constants.java
@@ -15,7 +15,7 @@ public final class Constants {
     private Constants() {}
     
     //Add some comment
-    //DavidHendy added another comment
+    //DavidHendy added another comment and changed it here too.
     public static final String VERSION = "1.1";
     
     public static final String OWNER = "Dave";
